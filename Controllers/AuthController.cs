3b6a38b29d70ca881d646bcf4c1a813b8fc9590f
WilsonCSilva <wilso.carvalhais@gmail.com>
2025-05-02 23:03:35 -0300

    02/05/25

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -1,9 +1,12 @@
-﻿using FCG.Infrastructure.Repository.Helpers;
-using FCG.Models;
+﻿using FCG.Models;
 using Infrastructure.Repository;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
-namespace FCG.Controllers
+namespace FIAPCloudGames.Controllers
 {
     [ApiController]
     [Route("[controller]")]
@@ -17,15 +20,31 @@ namespace FCG.Controllers
         {
             if (userName == "admin" && password == "admin")
             {
-                var token = GerarToken(userName, "admin");
+                var (token, dataCriacao, dataExpiracao) = GerarToken(userName, "admin");
+                
                 _logger.LogInfotmation("Token gerado com sucesso.");
-                return Ok(new { token });
+                
+                return Ok(new
+                {
+                    token,
+                    type = "Bearer",
+                    iat = dataCriacao.ToString("yyyy-MM-dd HH:mm:ss"),
+                    expires = dataExpiracao.ToString("yyyy-MM-dd HH:mm:ss")
+                });
             }
             else if (userName == "user" && password == "user")
             {
-                var token = GerarToken(userName, "user");
+                var (token, dataCriacao, dataExpiracao) = GerarToken(userName, "user");
+                
                 _logger.LogInfotmation("Token gerado com sucesso.");
-                return Ok(new { token });
+                
+                return Ok(new
+                {
+                    token,
+                    type = "Bearer",
+                    iat = dataCriacao.ToString("yyyy-MM-dd HH:mm:ss"),
+                    expires = dataExpiracao.ToString("yyyy-MM-dd HH:mm:ss")
+                });
             }
             else
             {
@@ -35,9 +54,40 @@ namespace FCG.Controllers
                     Erro = "Unauthorized",
                     Detalhe = "Usuário ou senha inválidos."
                 };
+                
                 _logger.LogError("Erro ao gerar token.");
+                
                 return Unauthorized(erroResponse);
             }
+        }
+
+        private (string token, DateTime dataCriacao, DateTime dataExpiracao) GerarToken(string userName, string role)
+        {
+            DateTime dataCriacao = DateTime.Now;
+            DateTime dataExpiracao = dataCriacao.AddMinutes(double.Parse(_configuration["JWT:ExpirationMinutes"]));
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JWT:Key"]));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, userName),
+                new Claim(JwtRegisteredClaimNames.Email, userName),
+                new Claim(ClaimTypes.Role, role),
+                new Claim(JwtRegisteredClaimNames.Iat, dataCriacao.ToString("yyyy-MM-dd HH:mm:ss")),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+            };
+
+            var token = new JwtSecurityToken(
+                issuer: _configuration["JWT:Issuer"],
+                audience: _configuration["JWT:Audience"],
+                claims: claims,
+                expires: dataExpiracao,
+                signingCredentials: creds
+            );
+
+            var tokenString = new JwtSecurityTokenHandler().WriteToken(token);
+            return (tokenString, dataCriacao, dataExpiracao);
         }
     }
 }
---
+++ Controllers/UsuarioController.cs
@@ -0,0 +1,16 @@
+﻿using Infrastructure.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FCG.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UsuarioController(IConfiguration configuratios, BaseLogger<UsuarioController> Logger) : ControllerBase
+    {
+        //private readonly IConfiguration _configuration = configuratios;
+        //private readonly BaseLogger<UsuarioController> _logger = Logger;
+
+        //[HttpGet("usuarios")]
+
+    }
+}
